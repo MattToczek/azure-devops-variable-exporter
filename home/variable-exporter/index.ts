f5066e4f682939f1daacd8f91db91321190d20d0
MattToczek <toczek.matt@gmail.com>
2023-03-31 12:11:37 +0100

    add secret and output options for vars

--- home/variable-exporter/index.ts
+++ home/variable-exporter/index.ts
@@ -18,11 +18,11 @@ function setVariables(jsonObj: {[key:string]: any}){
     return {keys: keys, vals: vals}
 }
 
-function outputVars(varObject: {keys:string[], vals:any[]}, outputSuffix: string, previousVarName: string = ""){
+function outputVars(varObject: {keys:string[], vals:any[]}, outputSuffix: string, secretSuffix: string, previousVarName: string = ""){
     for ( var i: number = 0; i <= varObject.keys.length; i ++ ){
         const currentVarName: string = `${previousVarName}${varObject.keys[i]}`
         if ( typeof varObject.vals[i] == "string" || typeof varObject.vals[i] == "number" || typeof varObject.vals[i] == "boolean" ) {
-            console.log(`##vso[task.setvariable variable=${currentVarName};]${varObject.vals[i]}${outputSuffix}`)
+            console.log(`##vso[task.setvariable variable=${currentVarName};${outputSuffix}${secretSuffix}]${varObject.vals[i]}`)
         } else if (Array.isArray(varObject.vals[i])) {
             const nestedKeys = [...varObject.vals[i].keys()]
             const nestedKeysStr = nestedKeys.map(String)
@@ -40,7 +40,9 @@ async function run() {
     try {
         const inputString: string | undefined = tl.getInput('variablesource', true);
         const output: boolean | false = tl.getBoolInput('output', false);
-        const outputSuffix: string = output ? ";isOutput=true" : ""
+        const secret: boolean | false = tl.getBoolInput('secret', false);
+        const outputSuffix: string = output ? "isOutput=true;" : ""
+        const secretSuffix: string = secret ? "issecret=true;" : ""
 
         if (inputString == 'bad') {
             tl.setResult(tl.TaskResult.Failed, 'No file input found!');
@@ -50,7 +52,7 @@ async function run() {
             jsonProm.then((value) => {
                 const parsedJson = JSON.parse(value)
                 const keyVal: {keys:string[], vals:any[]} = setVariables(parsedJson)
-                outputVars(keyVal, outputSuffix)
+                outputVars(keyVal, outputSuffix, secretSuffix)
             });
         }
     }
@@ -60,7 +62,6 @@ async function run() {
           } else {
             tl.setResult(tl.TaskResult.Failed, "Issue surfacing error message!");
           }
-        
     }
 }
 
